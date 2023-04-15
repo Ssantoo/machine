4572efd7dc9ab75a0568381e77d6a273d6c743df
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-04-15 14:02:31 +0900

    리팩토링

--- draw/src/main/java/com/example/draw/Drawing.java
+++ draw/src/main/java/com/example/draw/Drawing.java
@@ -8,11 +8,15 @@ import java.util.Random;
 
 public class Drawing {
     private final List<Item> items;
+
+    private static final double A_GRADE_CHANCE = 0.9;
     private static final int PRICE = 100;
     private int bGradeDrawnCount;
     private final LocalDateTime currentTime;
     private Wallet wallet;
     private final Random random;
+
+
 
     public Drawing(List<Item> items) {
         this.items = items;
@@ -30,38 +34,41 @@ public class Drawing {
 
     public void drawAction(List<Object> drawnResults) {
         wallet.useCash(PRICE);
-        drawnResults(drawnResults);
+        Item drawnItem = drawItem();
+        drawnResults(drawnResults, drawnItem);
     }
 
-    public void drawnResults(List<Object> drawnResults) {
-        Item drawnItem = drawItem();
+    public void drawnResults(List<Object> drawnResults, Item drawnItem) {
 
         // 꽝일 경우
         if (drawnItem == null) {
             drawnResults.add("꽝");
+        } else {
+            // B등급일 경우
+             if ("B".equals(drawnItem.getGrade())) {
+                 bGradeDrawnCount++;
+             }
+            drawnResults.add(drawnItem);
         }
-
-        // B등급일 경우
-        if ("B".equals(drawnItem.getGrade())) {
-            bGradeDrawnCount++;
-        }
-        drawnResults.add(drawnItem);
     }
 
     // 상품 뽑는 로직
     private Item drawItem() {
-        Item result = null;
+
         double drawChance = random.nextDouble();
 
-        if (drawChance < 0.9) {
-            result = drawItemByGrade("A");
+        if (drawChance < 0.5) {
+            double prizeChance = random.nextDouble();
+            if (bGradeDrawnCount < 3 && prizeChance >= A_GRADE_CHANCE) {
+                return drawItemByGrade("B");
+            } else {
+                return drawItemByGrade("A");
+            }
+        } else {
+            return null;
         }
-        if (bGradeDrawnCount < 3) {
-            result = drawItemByGrade("B");
-        }
-
-        return result;
     }
+
 
     // 등급별 상품을 뽑는 로직
     private Item drawItemByGrade(String grade) {
@@ -72,7 +79,8 @@ public class Drawing {
             }
         }
 
-
         return availableItems.get(0);
     }
+
+
 }
